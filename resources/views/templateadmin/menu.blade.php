757b40affd83e3219564f0351c2d6ad081aefc21
iboonkhet@gmail.com <123219570+Rkknoob1@users.noreply.github.com>
2023-02-20 17:25:43 +0700

    update
    update

--- resources/views/templateadmin/menu.blade.php
+++ resources/views/templateadmin/menu.blade.php
@@ -8,18 +8,12 @@
     <ul class="nav">
       <li class="nav-item">
         <div class="d-flex sidebar-profile">
-          <div class="sidebar-profile-image">
-            <img src="{{ asset('cms/images/faces/face29.png') }}" alt="image">
-
-            <span class="sidebar-status-indicator"></span>
-          </div>
           <div class="sidebar-profile-name">
             <p class="sidebar-name">
                 @auth
                 {{Auth::user()->name}}
                 @endauth
             </p>
-
           </div>
         </div>
 
