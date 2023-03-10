99af1d727430330d424ca32c5bbceafdbed27364
iboonkhet@gmail.com <123219570+Rkknoob1@users.noreply.github.com>
2023-03-10 10:51:27 +0700

    update
    update

--- resources/js/components/environment/environment.js
+++ resources/js/components/environment/environment.js
@@ -2,15 +2,15 @@
 // export const BASE_URL = 'http://restau.test/';  //base
 // export const IMAGE_URL = 'http://restau.test/public/product/';  //image
 
-// export const API_URL = 'https://restu.idtest.work/api/';  //api
-// export const BASE_URL = 'https://restu.idtest.work/';  //base
-// export const IMAGE_URL = 'https://restu.idtest.work/public/product/';  //image
+export const API_URL = 'https://restu.idtest.work/api/';  //api
+export const BASE_URL = 'https://restu.idtest.work/';  //base
+export const IMAGE_URL = 'https://restu.idtest.work/public/product/';  //image
 // export const API_URL = 'http://restu.test/api/';  //api
 // export const BASE_URL = 'http://restu.test/';  //base
 // export const IMAGE_URL = 'http://restu.test/public/product/';  //image
 
 
-export const API_URL = 'https://tarabeach-ud.com/api/';  //api
-export const BASE_URL = 'https://tarabeach-ud.com/';  //base
-export const IMAGE_URL = 'https://tarabeach-ud.com/public/product/';  //image
+// export const API_URL = 'https://tarabeach-ud.com/api/';  //api
+// export const BASE_URL = 'https://tarabeach-ud.com/';  //base
+// export const IMAGE_URL = 'https://tarabeach-ud.com/public/product/';  //image
 
